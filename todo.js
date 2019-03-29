f725fcaa6acecf62cd4adcfec6da03aa9923b470
Kamil <kamil.tomala@ath.pl>
2019-03-29 08:59:41 +0100

    Add comments to functions

--- todo.js
+++ todo.js
@@ -4,25 +4,25 @@ function displayTodos() {
     console.log('My todos: ', todos);
 }
 
-displayTodos();
+displayTodos(); // item 1, item 2, item 3, item 4
 
 function addTodo(todo) {
     todos.push(todo);
     displayTodos();
 }
 
-addTodo('item 5');
+addTodo('item 5'); // item 1, item 2, item 3, item 4, item 5
 
 function changeTodo(position, newValue) {
     todos[position] = newValue;
     displayTodos();
 }
 
-changeTodo(0, 'new item 1');
+changeTodo(0, 'new item 1'); // new item 1, item 2, item 3, item 4, item 5
 
 function deleteTodo(position) {
     todos.splice(position, 1);
     displayTodos();
 }
 
-deleteTodo(-1);  // item 5
+deleteTodo(-1);  // new item 1, item 2, item 3, item 4, item 5

f08101889d4def66a598b4695c0b9ecec6d452c5
Kamil <kamil.tomala@ath.pl>
2019-03-29 14:36:09 +0100

    Teggle all todos to true

--- script.js
+++ script.js
@@ -48,6 +48,10 @@ let todoList = {
             for (let i = 0; i < totalTodos; i++) {
                 this.todos[i].complete = false;
             }
+        } else {
+            for (let i = 0; i < totalTodos; i++) {
+                this.todos[i].complete = true;
+            }
         }
 
         this.displayTodos();

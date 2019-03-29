8989afdd83149c183b4ade5b48d19ae2919f6521
Kamil <kamil.tomala@ath.pl>
2019-03-29 12:26:25 +0100

    Add new method toggleComplete with boolean value

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let todoList = {
-    todos: ['item 1', 'item 2', 'item 3', 'item 4'],
+    todos: [],
     displayTodos: function() {
         console.log('My Todos: ', this.todos);
     },
-    addTodo: function(todo) {
-        this.todos.push(todo);
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            complete: false
+        });
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleComplete: function(position) {
+        let todo = this.todos[position];
+        todo.complete = !todo.complete;
         this.displayTodos();
     }
 };

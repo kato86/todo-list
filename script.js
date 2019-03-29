610064ac6f92256b50d868b00bf269276a6b0abb
Kamil <kamil.tomala@ath.pl>
2019-03-29 10:40:36 +0100

    Add addTodo method

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ let todoList = {
     todos: ['item 1', 'item 2', 'item 3', 'item 4'],
     displayTodos: function() {
         console.log('My Todos: ', this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
     }
 };
 
-console.log(todoList);
-console.log(todoList.displayTodos());
-
-// function addTodo(todo) {
-//     todos.push(todo);
-//     displayTodos();
-// }
+console.log(todoList.addTodo('item 5'));
 
 // addTodo('item 5'); // item 1, item 2, item 3, item 4, item 5
 

dfa3bd7f51fcdb02e7c83dfafe48032bf187f3df
Kamil <kamil.tomala@ath.pl>
2019-04-03 14:29:05 +0200

    Refactoring - buttons Display Todos, Toggle All

--- script.js
+++ script.js
@@ -58,13 +58,11 @@ let todoList = {
     }
 };
 
-let displayTodosButton = document.getElementById('displayTodosButton');
-let toggleAllButton = document.getElementById('toggleAllButton');
-
-displayTodosButton.addEventListener('click', function() {
-    todoList.displayTodos();
-});
-
-toggleAllButton.addEventListener('click', function() {
-    todoList.toggleAll();
-});
+let handlers = {
+    displayTodos: function() {
+        todoList.displayTodos();
+    },
+    toggleAll: function() {
+        todoList.toggleAll();
+    }
+};
